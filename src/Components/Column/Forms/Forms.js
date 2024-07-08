b8812f79313e35bde289a57e86336716b4ddda5c
KesavanSaravanan13 <kesavan.saravanan@concertidc.com>
2024-07-08 10:36:39 +0530

    changes in width

--- src/Components/Column/Forms/Forms.js
+++ src/Components/Column/Forms/Forms.js
@@ -10,30 +10,32 @@ const Forms = () => {
                 <div className='row m-0 p-0'>
                     <div className='col-8 m-0 p-0 flex-grow-1 pDetails'>Personal Details</div>
                     <div className='col-4 m-0 p-0 text-end'>
-                        <div className='row m-0 p-0 justify-content-end'>
-                            <div className='col-1 m-0 p-0 px-2 edit'><img src={edit} className='m-0 p-0'></img></div>
-                            <div className='col-1 m-0 p-0 edit'>Edit</div>
-                        </div>
+                        <button className='m-0 p-0 btn editBtn'>
+                            <div className='row m-0 p-0 justify-content-end'>
+                                <div className='col-1 m-0 p-0 px-2 edit'><img src={edit} className='m-0 p-0'></img></div>
+                                <div className='col-1 m-0 p-0 edit'>Edit</div>
+                            </div>
+                        </button>
                     </div>
                 </div>
                 <div className='row m-0 p-0'>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>First Name</label>
-                        <input type=" text" className='col-12 m-0 p-0' placeholder='Jessica'></input>
+                        <input type=" text" className='col-12 m-0 p-0 w-100' placeholder='Jessica'></input>
                     </div>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>Last Name</label>
-                        <input type=" text " className='col-12 m-0 p-0' placeholder='Mark'></input>
+                        <input type=" text " className='col-12 m-0 p-0 w-100' placeholder='Mark'></input>
                     </div>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>Email Address</label>
-                        <input type="email" className='col-12 m-0 p-0' placeholder='jessica@gmail.com'></input>
+                        <input type="email" className='col-12 m-0 p-0 w-100' placeholder='jessica@gmail.com'></input>
                     </div>
                 </div>
                 <div className='row m-0 p-0'>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>Gender</label>
-                        <select className='form-select m-0 col-12 m-0 gdiv d-flex align-items-center'>
+                        <select className='form-select m-0 col-12 m-0 gdiv d-flex align-items-center w-100'>
                             <option value="Male">Male</option>
                             <option value="Female" selected>Female</option>
                             <option value="Others">Other</option>
@@ -42,7 +44,7 @@ const Forms = () => {
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>DOB</label>
                         <div className='col-12 m-0 p-0 gdiv d-flex align-items-center'>
-                            <div className='row m-0 p-0 flex-fill justify-content-between'>
+                            <div className='row m-0 p-0 flex-fill justify-content-between w-100'>
                                 <input type=" text" className=' m-0 p-0 gender' placeholder='Dec 13, 1989'></input>
                                 <div className='col-1 m-0 p-0 me-2'>
                                     <img src={dob} class=" m-0 p-0"
@@ -53,27 +55,27 @@ const Forms = () => {
                     </div>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>Phone Number</label>
-                        <input type="number" className='col-12 m-0 p-0' placeholder='+1 (201) 874 8486'></input>
+                        <input type="number" className='col-12 m-0 p-0 w-100' placeholder='+1 (201) 874 8486'></input>
                     </div>
                 </div>
                 <div className='row m-0 p-0'>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>Address</label>
-                        <input type=" text" className='col-12 m-0 p-0' placeholder='1027 Edgardo'></input>
+                        <input type=" text" className='col-12 m-0 p-0 w-100' placeholder='1027 Edgardo'></input>
                     </div>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>City</label>
-                        <input type=" text" className='col-12 m-0 p-0' placeholder='Arlington'></input>
+                        <input type=" text" className='col-12 m-0 p-0 w-100' placeholder='Arlington'></input>
                     </div>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>State</label>
-                        <input type=" text" className='col-12 m-0 p-0' placeholder='Virginia'></input>
+                        <input type=" text" className='col-12 m-0 p-0 w-100' placeholder='Virginia'></input>
                     </div>
                 </div>
                 <div className='row m-0 p-0'>
                     <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                         <label className='col-12 m-0 p-0 pb-2'>Postal code</label>
-                        <input type=" text" className='col-12 m-0 p-0' placeholder='22201'></input>
+                        <input type=" text" className='col-12 m-0 p-0 w-100' placeholder='22201'></input>
                     </div>
                 </div>
             </forms>
