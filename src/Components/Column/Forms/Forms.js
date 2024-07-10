4e18f77dfc5e57473725da19a471c0f838f3701c
KesavanSaravanan13 <kesavan.saravanan@concertidc.com>
2024-07-10 18:34:17 +0530

    fixed bugs

--- src/Components/Column/Forms/Forms.js
+++ src/Components/Column/Forms/Forms.js
@@ -1,10 +1,8 @@
 import './Forms.css';
 import edit from '../assets/edit-text.png';
-import dropdown from '../assets/dropdown_arrow.webp';
 import dob from '../assets/dob.png';
 import { useState } from 'react';
 import Save from './Submit';
-import Props from '../../Toast/Toast';
 import ActiveBar from '../ActiveBar/ActiveBar';
 import TopRow from '../TopRow/TopRow';
 
@@ -17,7 +15,13 @@ const Forms = () => {
     const [inputCity, setinputCity] = useState('');
     const [inputState, setinputState] = useState('');
     const [inputPostalCode, setinputPostalCode] = useState('');
+    const [valueDisable, setvalueDisable] = useState(true);
+    const [saveDisable, setSaveDisable] = useState(false);
 
+    const handledisable = () => {
+        setvalueDisable(!valueDisable);
+        // setSaveDisable(!saveDisable);
+    }
     const handleFirstName = (event) => {
         setinputFirstName(event.target.value);
     }
@@ -55,13 +59,9 @@ const Forms = () => {
                         <div className='col-8 m-0 p-0 flex-grow-1 pDetails'>Personal Details</div>
                         <div className='col-4 m-0 p-0 text-end'>
                             <button className='m-0 p-0 btn editBtn' onClick={() => {
-                                <Props message='Edit is Enabled!!!' />
                                 alert(`You have enabled the EDIT!!!`);
-                                var inputs = document.querySelectorAll("#formEle input");
-                                inputs.forEach(function (input) {
-                                    input.disabled = false;
-                                });
-                                document.getElementById("submitBtn").style.display = 'flex';
+                                handledisable();
+                                document.getElementById("submitBtn").style.display='flex';
                             }}>
                                 <div className='row m-0 p-0 justify-content-between'>
                                     <div className='col-1 m-0 p-0 px-1 edit'><img src={edit} className='m-0 p-0' /></div>
@@ -73,21 +73,21 @@ const Forms = () => {
                     <div className='row m-0 p-0'>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>First Name</label>
-                            <input type="text" className='col-12 m-0 w-100' disabled placeholder='First Name' value={inputFirstName} onChange={handleFirstName}></input>
+                            <input type="text" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='First Name' value={inputFirstName} onChange={handleFirstName}></input>
                         </div>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>Last Name</label>
-                            <input type="text" className='col-12 m-0 w-100' disabled placeholder='Last Name' value={inputLasttName} onChange={handleLastName}></input>
+                            <input type="text" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='Last Name' value={inputLasttName} onChange={handleLastName}></input>
                         </div>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>Email Address</label>
-                            <input type="email" className='col-12 m-0 w-100' disabled placeholder='Email Address' value={inputMail} onChange={handleMail}></input>
+                            <input type="email" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='Email Address' value={inputMail} onChange={handleMail}></input>
                         </div>
                     </div>
                     <div className='row m-0 p-0'>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>Gender</label>
-                            <select className='m-0 col-12 m-0 gdiv d-flex align-items-center w-100' disabled>
+                            <select className='m-0 col-12 m-0 gdiv d-flex align-items-center w-100' disabled={valueDisable}>
                                 <option value="Male">Male</option>
                                 <option value="Female" selected>Female</option>
                                 <option value="Others">Other</option>
@@ -97,7 +97,7 @@ const Forms = () => {
                             <label className='col-12 m-0 p-0 pb-2'>DOB</label>
                             <div className='col-12 m-0 p-0 gdiv d-flex align-items-center dob'>
                                 <div className='row m-0 p-0 flex-fill justify-content-between w-100'>
-                                    <input type=" text" className=' m-0 gender dobinput' disabled placeholder='Dec 13, 1989' value=''></input>
+                                    <input type=" text" className=' m-0 gender dobinput' disabled={valueDisable} placeholder='Dec 13, 1989' value=''></input>
                                     <div className='col-1 m-0 p-0 me-2'>
                                         <img src={dob} class=" m-0 p-0"
                                             width="20px" height="15px" alt=""></img>
@@ -107,30 +107,30 @@ const Forms = () => {
                         </div>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>Phone Number</label>
-                            <input type="number" className='col-12 m-0 w-100' disabled placeholder='Phone Number' value={inputNumber} onChange={handleNumber}></input>
+                            <input type="number" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='Phone Number' value={inputNumber} onChange={handleNumber}></input>
                         </div>
                     </div>
                     <div className='row m-0 p-0'>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>Address</label>
-                            <input type=" text" className='col-12 m-0 w-100' disabled placeholder='Address' value={inputAddress} onChange={handleAddress}></input>
+                            <input type=" text" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='Address' value={inputAddress} onChange={handleAddress}></input>
                         </div>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>City</label>
-                            <input type=" text" className='col-12 m-0 w-100' disabled placeholder='City' value={inputCity} onChange={handleCity}></input>
+                            <input type=" text" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='City' value={inputCity} onChange={handleCity}></input>
                         </div>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>State</label>
-                            <input type=" text" className='col-12 m-0 w-100' disabled placeholder='State' value={inputState} onChange={handleState}></input>
+                            <input type=" text" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='State' value={inputState} onChange={handleState}></input>
                         </div>
                     </div>
                     <div className='row m-0 p-0'>
                         <div className='col-12 col-md-4 col-lg-4 m-0 p-2 py-3'>
                             <label className='col-12 m-0 p-0 pb-2'>Postal code</label>
-                            <input type=" text" className='col-12 m-0 w-100' disabled placeholder='Postal Code' value={inputPostalCode} onChange={handlePostalCode}></input>
+                            <input type=" text" className='col-12 m-0 w-100' disabled={valueDisable} placeholder='Postal Code' value={inputPostalCode} onChange={handlePostalCode}></input>
                         </div>
                     </div>
-                    <Save id='submitBtn' name={inputFirstName} email={inputMail} />
+                    <Save id='submitBtn' funct={handledisable}/>
                 </forms>
             </div>
         </div>
--- src/Components/Column/Forms/Submit.js
+++ src/Components/Column/Forms/Submit.js
@@ -1,32 +1,16 @@
 import './Submit.css';
-
-let n;
-let m;
 const Submit = (props) => {
     return (
         <div className='m-0 p-0 row justify-content-end' id='submitBtn' onClick={
             () => {
                 alert(`Your changes have SAVED!!!`);
-                var inputs = document.querySelectorAll("#formEle input");
-
-                inputs.forEach(function (input) {
-                    input.disabled = true;
-                });
-                n=props.name;
-                m=props.email
-
+                props.funct();
                 document.getElementById("submitBtn").style.display='none';
-               
             }
         }>
             <button type="buttton" value="Save" className="m-0 p-2 saveBtn" >Save</button>
         </div>
     );
-}
-
-function submit(){
-
-    return {n,m};
 }
 
 export default Submit;
